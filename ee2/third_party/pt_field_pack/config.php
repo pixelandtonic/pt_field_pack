39a4949460af6a2120ea1a79c8ec1cd3912e484a
Brandon Kelly <me@brandon-kelly.com>
2010-09-25 06:12:28 -0700

    1.0.2

--- ee2/third_party/pt_field_pack/config.php
+++ ee2/third_party/pt_field_pack/config.php
@@ -1,5 +1,5 @@
 <?php
 
 $config['name']    = 'P&T Field Pack';
-$config['version'] = '1.0.1';
+$config['version'] = '1.0.2';
 $config['nsm_addon_updater']['versions_xml'] = 'http://pixelandtonic.com/pt_field_pack/releasenotes.rss';
